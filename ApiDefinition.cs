3e9700f1d3cd47ca581c1dde1fa51cb0bf009e26
Safwan Choudhury <im@safwanc.com>
2014-07-28 17:07:35 -0400

    Expanded support for Blue SC sensor

--- ApiDefinition.cs
+++ ApiDefinition.cs
@@ -38,10 +38,6 @@ namespace WFConnector
         [Export("currentState")]
         WFHardwareConnectorState CurrentState { get; } 
 
-        [Static]
-        [Export("sharedConnector")]
-        WFHardwareConnector SharedConnector { get; }
-
         [Export("discoverDevicesOfType:onNetwork:searchTimeout:")]
         bool DiscoverDevices(WFSensorType SensorType, WFNetworkType NetworkType, double SearchTimeout); 
 
@@ -62,31 +58,358 @@ namespace WFConnector
 
         [Export("requestSensorConnection:withProximity:")]
         WFSensorConnection RequestSensorConnection(WFConnectionParams Params, WFProximityRange Proximity); 
+
+        [Export("requestSensorConnection:withProximity:error:")]
+        WFSensorConnection RequestSensorConnection(WFConnectionParams Params, WFProximityRange Proximity, NSError Error);
+
+        [Export("resetConnections")]
+        bool ResetConnections(); 
+
+        [Export("resetSampleTimer")]
+        void ResetSampleTimer(); 
+
+        [Export("setSampleTimerDataCheck:")]
+        void SetSampleTimerDataCheck(bool CheckData); 
+
+        [Export("prepareForBackground")]
+        void PrepareForBackground(); 
+
+        [Export("returnFromBackground")]
+        void ReturnFromBackground(); 
+
+        [Export("resetAllSensorData")]
+        bool ResetAllSensorData(); 
+
+        [Static]
+        [Export("sharedConnector")]
+        WFHardwareConnector SharedConnector { get; }
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface WFDeviceParams
+    {
+        [Export("networkType")]
+        WFNetworkType NetworkType { get; } 
+
+        [Export("deviceIDString")]
+        NSString DeviceIDString { get; }
+
+        [Export("deviceUUIDString")]
+        NSString DeviceUUIDString { get; }
+
+        [Export("deviceNumber")]
+        short DeviceNumber { get; }
+
+        [Export("transmissionType")]
+        char TransmissionType { get; }
+
+        [Export("isValid")]
+        bool IsValid(); 
     }
 
     [BaseType(typeof(NSObject))]
     interface WFConnectionParams
     {
-        // TODO
+        [Export("device1")]
+        WFDeviceParams Device1 { get; }
+
+        [Export("device2")]
+        WFDeviceParams Device2 { get; }
+
+        [Export("device3")]
+        WFDeviceParams Device3 { get; }
+
+        [Export("device4")]
+        WFDeviceParams Device4 { get; }
+
+        [Export("conditionalComparisonType")]
+        WFComparisonType ConditionalComparisonType { get; set; }
+
+        [Export("conditionalData")]
+        NSData ConditionalData { get; set; }
+
+        [Export("isConnectionConditional")]
+        bool IsConnectionConditional { get; }
+
+        [Export("isWildcard")]
+        bool IsWildcard { get; }
+
+        [Export("networkType")]
+        WFNetworkType NetworkType { get; set; }
+
+        [Export("searchTimeout")]
+        short SearchTimeout { get; set; }
+
+        [Export("sensorType")]
+        WFSensorType SensorType { get; set; }
+
+        [Export("sensorSubType")]
+        WFSensorSubType SensorSubType { get; set; }
+
+        [Export("hasDeviceNumber:")]
+        bool HasDeviceNumber(short DeviceNumber); 
+
+        [Export("hasDeviceUUID:")]
+        bool HasDeviceUUID(NSString UUIDString); 
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface WFConnectorSettings
+    {
+        [Export("bikeCoastingTimeout")]
+        double BikeCoastingTimeout { get; set; }
+
+        [Export("bikeWheelCircumference")]
+        float BikeWheelCircumference { get; set; }
+
+        [Export("searchTimeout")]
+        double SearchTimeout { get; set; }
+
+        [Export("discoveryTimeout")]
+        double DiscoveryTimeout { get; set; }
+
+        [Export("staleDataString")]
+        NSString StaleDataString { get; set; }
+
+        [Export("staleDataTimeout")]
+        double StaleDataTimeout { get; set; }
+
+        [Export("useMetricUnits")]
+        bool UseMetricUnits { get; set; }
+
+        [Export("connectionParamsForSensorType:")]
+        WFConnectionParams ConnectionParamsForSensorType(WFSensorType SensorType); 
+
+        [Export("deviceParamsForSensorType:")]
+        NSArray DeviceParamsForSensorType(WFSensorType SensorType); 
+
+        [Export("removeDeviceParams:forSensorType:")]
+        bool RemoveDeviceParamsForSensorType(WFDeviceParams DeviceParams, WFSensorType SensorType); 
+
+        [Export("saveConnectionInfo:")]
+        bool SaveConnectionInfo(WFSensorConnection ConnectionInfo); 
+    }
+
+    [Protocol]
+    [BaseType(typeof(NSObject))]
+    interface WFSensorConnectionDelegate
+    {
+        [Export("connectionDidTimeout:")]
+        void ConnectionDidTimeout(WFSensorConnection ConnectionInfo); 
+
+        [Export("connection:stateChanged:")]
+        void ConnectionStateChanged(WFSensorConnection ConnectionInfo, WFSensorConnectionStatus ConnectionState); 
+
+        [Export("connection:rejectedByDeviceNamed:appAlreadyConnected:")]
+        void ConnectionRejected(WFSensorConnection ConnectionInfo, NSString DeviceName, NSString AlreadyConnectedAppName); 
     }
 
     [BaseType(typeof(NSObject))]
     interface WFSensorConnection
     {
-        // TODO
+        [Export("connectionStatus")]
+        WFSensorConnectionStatus ConnectionStatus { get; }
+
+        [Export("delegate")]
+        WFSensorConnectionDelegate Delegate { get; set; }
+
+        [Export("deviceIDString")]
+        NSString DeviceIDString { get; }
+
+        [Export("deviceNumber")]
+        short DeviceNumber { get; }
+
+        [Export("deviceUUIDString")]
+        NSString DeviceUUIDString { get; }
+
+        [Export("didTimeout")]
+        bool DidTimeout { get; }
+
+        [Export("error")]
+        WFSensorConnectionError Error { get; }
+
+        [Export("isConnected")]
+        bool IsConnected { get; }
+
+        [Export("isBTLEConnection")]
+        bool IsBTLEConnection { get; }
+
+        [Export("isANTConnection")]
+        bool IsANTConnection { get; }
+
+        [Export("isValid")]
+        bool IsValid { get; }
+
+        [Export("hasError")]
+        bool HasError { get; }
+
+        [Export("hasValidParams")]
+        bool HasValidParams { get; }
+
+        [Export("hasWildcardParams")]
+        bool HasWildcardParams { get; }
+
+        [Export("networkType")]
+        WFNetworkType NetworkType { get; }
+
+        [Export("sensorType")]
+        WFSensorType SensorType { get; }
+
+        [Export("sensorSubType")]
+        WFSensorSubType SensorSubType { get; }
+
+        [Export("timeSinceLastMessage")]
+        double TimeSinceLastMessage { get; }
+
+        [Export("transmissionType")]
+        char TransmissionType { get; }
+
+        [Export("disconnect")]
+        void Disconnect(); 
+
+        [Export("getData")]
+        WFSensorData GetData(); 
+
+        [Export("getRawData")]
+        WFSensorData GetRawData(); 
+
+        [Export("hasData")]
+        bool HasData(); 
+
+        [Export("signalEfficiency")]
+        float SignalEfficiency { get; }
     }
 
     [BaseType(typeof(NSObject))]
+    interface WFOdometerHistory
+    {
+        [Export("bikeWheelCircumference")]
+        float BikeWheelCircumference { get; set; }
+
+        [Export("ulCurrentWheelRevolutions")]
+        ulong CurrentWheelRevolutions { get; }
+
+        [Export("getDistanceForWeek:")]
+        float GetDistanceForWeek(char Week); 
+
+        [Export("getOdometerForWeek:")]
+        float GetOdometerForWeek(char Week); 
+
+        [Export("getWheelRevolutionsForWeek:")]
+        ulong GetWheelRevolutionsForWeek(char Week); 
+    }
+
+    [Protocol]
+    [BaseType(typeof(NSObject))]
+    interface WFBikeSpeedCadenceDelegate
+    {
+        [Export("cscConnection:didReceiveOdometerHistory:")]
+        void DidReceiveOdometerHistory(WFBikeSpeedCadenceConnection Connection, WFOdometerHistory History); 
+
+        [Export("cscConnection:didResetOdometer:")]
+        void DidResetOdometer(WFBikeSpeedCadenceConnection Connection, bool Success); 
+
+        [Export("cscConnection:didReceiveGearRatio:numerator:denominator:")]
+        void DidReceiveGearRatio(WFBikeSpeedCadenceConnection Connection, bool Success, short Numerator, short Denominator); 
+
+        [Export("cscConnection:didReceiveSetGearRatioResponse:")]
+        void DidReceiveSetGearRatioResponse(WFBikeSpeedCadenceConnection Connection, bool Success); 
+    }
+
+    [BaseType(typeof(WFSensorConnection))]
     interface WFBikeSpeedCadenceConnection
     {
-        // TODO
+        [Export("cscDelegate")]
+        WFBikeSpeedCadenceDelegate Delegate { get; set; }
+
+        [Export("getBikeSpeedCadenceData")]
+        WFBikeSpeedCadenceData GetBikeSpeedCadenceData(); 
+
+        [Export("getBikeSpeedCadenceRawData")]
+        WFBikeSpeedCadenceData GetBikeSpeedCadenceRawData(); 
+
+        [Export("getCSCData")]
+        WFBTLEBikeSpeedCadenceData GetCSCData(); 
+
+        [Export("requestOdometerHistoryFrom:to:")]
+        bool RequestOdometerHistory(char From, char To); 
+
+        [Export("requestOdometerReset:")]
+        bool RequestOdometerReset(float ResetValue);
+
+        [Export("setDeviceTime")]
+        bool SetDeviceTime(); 
+
+        [Export("setDeviceGearRatioWithNumerator:andDenomonator:")]
+        bool SetDeviceGearRatio(short Numerator, short Denominator); 
+
+        [Export("getDeviceGearRatio")]
+        bool GetDeviceGearRatio(); 
+
+        // NOTE: Ignoring the following commands - 
+        // - (BOOL)sendRecordCommand:(WFBTLEWahooCSCOpCode_t)opCode withOperator:(WFBTLERecordOperator_t)op operands:(NSData*)operands;
+    }
+
+    [BaseType(typeof(NSObject))]
+    interface WFSensorData
+    {
+        [Export("isDataStale")]
+        bool IsDataStale { get; }
+
+        [Export("settings")]
+        WFConnectorSettings Settings { get; }
+
+        [Export("initWithTime:")]
+        NSObject InitWithTime(double DataTime); 
     }
 
     [BaseType(typeof(NSObject))]
     interface WFBikeSpeedCadenceData
     {
-        // TODO
+        [Export("accumCrankRevolutions")]
+        ulong AccumCrankRevolutions { get; }
+
+        [Export("accumCadenceTime")]
+        float AccumCadenceTime { get; }
+
+        [Export("instantCrankRPM")]
+        char InstantCrankRPM { get; }
+
+        [Export("cadenceTimestamp")]
+        double CadenceTimestamp { get; }
+
+        [Export("cadenceTimestampOverflow")]
+        bool CadenceTimestampOverflow { get; }
+
+        [Export("accumWheelRevolutions")]
+        ulong AccumWheelRevolutions { get; }
+
+        [Export("accumSpeedTime")]
+        float AccumSpeedTime { get; }
+
+        [Export("instantWheelRPM")]
+        short InstantWheelRPM { get; }
+
+        [Export("speedTimestamp")]
+        double SpeedTimestamp { get; }
+
+        [Export("speedTimestampOverflow")]
+        bool SpeedTimestampOverflow { get; }
+
+        [Export("formattedCadence:")]
+        NSString FormattedCadence(bool ShowUnits); 
+
+        [Export("formattedDistance:")]
+        NSString FormattedDistance(bool ShowUnits); 
+
+        [Export("formattedSpeed:")]
+        NSString FormattedSpeed(bool ShowUnits); 
     }
 
+    [BaseType(typeof(WFBikeSpeedCadenceData))]
+    interface WFBTLEBikeSpeedCadenceData
+    {
+        // TODO
+    }
 }
 
--- StructsAndEnums.cs
+++ StructsAndEnums.cs
@@ -99,6 +99,32 @@ namespace WFConnector
         WF_SENSORTYPE_DISPLAY                        = 0x00800000,
     }
 
+    public enum WFSensorSubType
+    {
+        /** The sub-type is not specified. */
+        WF_SENSOR_SUBTYPE_UNSPECIFIED                = 0,
+
+        /** Specifies the Wahoo KICKR Bike Trainer. */
+        WF_SENSOR_SUBTYPE_BIKE_POWER_KICKR = 1,
+        /** Specifies the Stages StageOne Bike Power Meter. */
+        WF_SENSOR_SUBTYPE_BIKE_POWER_STAGE_ONE = 2,
+        /** Specifies the Kurt InRide Bike Trainer. */
+        WF_SENSOR_SUBTYPE_BIKE_POWER_IN_RIDE = 3,
+
+        /** Specifies the RFLKT watch display. */
+        WF_SENSOR_SUBTYPE_DISPLAY_RFLKT = 5,
+        /** Specifies a Megellan Echo Watch subtype */
+        WF_SENSOR_SUBTYPE_DISPLAY_ECHO = 6,
+
+        /** Specifies the Wahoo TICKR Heart Rate sensor. */
+        WF_SENSOR_SUBTYPE_HEARTRATE_TICKR = 4,
+        /** Specifies the Wahoo TICKR X Heart Rate sensor. */
+        WF_SENSOR_SUBTYPE_HEARTRATE_TICKR_X = 7,
+
+        /** Specifies the Casio Type1 Watch Eg. STB-1000 */
+        WF_SENSOR_SUBTYPE_DISPLAY_CASIO_TYPE1 = 8,
+    }
+
     [Flags]
     public enum WFSensorConnectionStatus
     {
@@ -112,6 +138,32 @@ namespace WFConnector
         WF_SENSOR_CONNECTION_STATUS_INTERRUPTED,
         /** The connection is in process of being disconnected. */
         WF_SENSOR_CONNECTION_STATUS_DISCONNECTING,
+    }
+
+    public enum WFSensorConnectionError
+    {
+        /** No error has occured. */
+        WF_SENSOR_CONN_ERROR_NONE  = 0,
+        /** Failed to retrieve the specified paired sensor. */
+        WF_SENSOR_CONN_ERROR_PAIRED_DEVICE_NOT_AVAILABLE,
+        /**
+         * Occurs when an attempt is made to initiate a proximity search while a
+         * sensor of the specified type is currently connected to the phone.
+         */
+        WF_SENSOR_CONN_ERROR_PROXIMITY_SEARCH_WHILE_CONNECTED,
+        /**
+         * Occurs on a conditional connection when no new data is available from the
+         * device.
+         */
+        WF_SENSOR_CONN_ERROR_NO_NEW_DATA,
+        /**
+         * Occurs when there has been no activity on the connection for the duration
+         * of the connection activity timeout.
+         *
+         * @note The activity timeout is device-type-specific and is not used on devices
+         * which require constant data updates.
+         */
+        WF_SENSOR_CONN_ERROR_ACTIVITY_TIMEOUT,
     }
 
     public enum WFProximityRange
@@ -138,5 +190,21 @@ namespace WFConnector
         WF_PROXIMITY_RANGE_9,
         /** Proximity range 10 of 10 (farthest range). */
         WF_PROXIMITY_RANGE_10,
+    }
+
+    public enum WFComparisonType
+    {
+        /** No comparison specified. */
+        WF_COMPARISON_TYPE_NONE = 0,
+        /** The comparison is <c>TRUE</c> if the item on the left is less than the item on the right. */
+        WF_COMPARISON_TYPE_LESS_THAN,
+        /** The comparison is <c>TRUE</c> if the item on the left is less than or equal to the item on the right. */
+        WF_COMPARISON_TYPE_LESS_THAN_OR_EQUAL,
+        /** The comparison is <c>TRUE</c> if the item on the left is equal to the item on the right. */
+        WF_COMPARISON_TYPE_EQUAL,
+        /** The comparison is <c>TRUE</c> if the item on the left is greater than or equal to the item on the right. */
+        WF_COMPARISON_TYPE_GREATER_THAN_OR_EQUAL,
+        /** The comparison is <c>TRUE</c> if the item on the left is greater than the item on the right. */
+        WF_COMPARISON_TYPE_GREATER_THAN,
     }
 }
